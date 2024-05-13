049b3af6cdce494705337e640d76ec9930c6c867
Giulia Cellerino <giulia.cellerino@icloud.com>
2024-05-13 14:47:32 +0200

    Starting to update the read.me file

--- model/init_db.sql
+++ model/init_db.sql
@@ -3,16 +3,22 @@
 --
 
 SET foreign_key_checks = 0;
-DROP TABLE if exists students;
+DROP TABLE if exists sales;
 SET foreign_key_checks = 1;
 
 --
 -- Create Tables
 --
 -- We need to subsitite this with the new table I ll create in https://drawsql.app/diagrams
-CREATE TABLE students(
-    id INT NOT NULL AUTO_INCREMENT, 
-    firstname VARCHAR(40) not null, 
-    lastname VARCHAR(40) not null, 
-    PRIMARY KEY (id)
-    );
+CREATE TABLE `Sales`(
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `day` DATE NOT NULL,
+    `income` DECIMAL(8, 2) NOT NULL,
+    `men` BIGINT NOT NULL,
+    `women` BIGINT NOT NULL,
+    `kids` BIGINT NOT NULL,
+    `clothing` BIGINT NOT NULL,
+    `sport` BIGINT NOT NULL,
+    `home` BIGINT NOT NULL,
+    `weather` VARCHAR(255) NOT NULL
+);
